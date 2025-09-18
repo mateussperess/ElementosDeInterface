1271cc8c5f7296d64df615185fd5d82c4a18c34b
mateussperess <mateuspereslopesl@outlook.com>
2025-09-18 20:20:55 -0300

    testando alguns componentes de interface

--- app/src/main/java/com/example/outroselementosinterface/MainActivity.java
+++ app/src/main/java/com/example/outroselementosinterface/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.outroselementosinterface;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    public CheckBox chkTeste;
+    public TextView txtCheck;
+
+    public Button btnRadioBtns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        chkTeste = findViewById(R.id.chkTeste);
+        txtCheck = findViewById(R.id.txtCheck);
+
+        chkTeste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (chkTeste.isChecked()) {
+                    txtCheck.setText("CheckBox selecionado");
+                } else {
+                    txtCheck.setText("CheckBox não selecionado");
+                }
+            }
+        });
+
+        btnRadioBtns = findViewById(R.id.btnRadioButtons);
+        btnRadioBtns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent itRadioBtns = new Intent(MainActivity.this, RadioBtns.class);
+                startActivity(itRadioBtns);
+            }
         });
     }
 }
---
+++ app/src/main/java/com/example/outroselementosinterface/RadioBtns.java
@@ -0,0 +1,55 @@
+package com.example.outroselementosinterface;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RadioBtns extends AppCompatActivity {
+    public TextView txtRadio;
+    public Button btnRadio;
+    public RadioGroup rdGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_radio_btns);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        txtRadio = findViewById(R.id.txtRadio);
+        btnRadio = findViewById(R.id.btnRadio);
+        rdGroup = findViewById(R.id.radioGroup);
+
+        btnRadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TESTE", String.valueOf(rdGroup.getCheckedRadioButtonId()));
+                if (rdGroup.getCheckedRadioButtonId() == R.id.rdBtn1) {
+                    txtRadio.setText("Radio 1");
+                }
+                if (rdGroup.getCheckedRadioButtonId() == R.id.rdBtn2) {
+                    txtRadio.setText("Radio 2");
+                }
+                if (rdGroup.getCheckedRadioButtonId() == R.id.rdBtn3) {
+                    txtRadio.setText("Radio 3");
+                }
+                if (rdGroup.getCheckedRadioButtonId() == R.id.rdBtn4) {
+                    txtRadio.setText("Radio 4");
+                }
+            }
+        });
+    }
+}
